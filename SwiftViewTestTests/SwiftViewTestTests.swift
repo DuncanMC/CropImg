498bb2645fa71baa174f632fe2a84b8fbb9cb537
Duncan Champney <DuncanC@aol.com>
2015-03-24 11:10:59 -0400

    Initial Commit

---
+++ SwiftViewTestTests/SwiftViewTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftViewTestTests.swift
+//  SwiftViewTestTests
+//
+//  Created by Duncan Champney on 3/24/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftViewTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
